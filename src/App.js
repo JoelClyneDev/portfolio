e21e4e639a96175ba126b03dc0fab9ce3d8c343f
JoelClyneDev <joel.clyne2@gmail.com>
2020-07-24 13:56:47 -0400

    Edited fonts and buttons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import profilePicture from './Red.svg.png';
+import './strings/websiteText';
+import WEBSITE_TEXT from './strings/websiteText';
 
 
 
 function App() {
 
+// for scrolling to projects button
+
+//for scrolling to an element stated in the ref
+const scrollToProjects = (ref) => window.scrollTo(0, ref.current.offsetTop);
+const projectRef = useRef(null)
+const executeScroll = () => scrollToProjects(projectRef)
+
+    
 
     const windowHeight = window.innerHeight;
     const windowWidth = window.innerWidth
     const profileAreaHeightForTranslation = 204 / 2;
+    const textInput = WEBSITE_TEXT;
+    console.log(textInput.NAME)
 
 
     return ( 
@@ -21,14 +33,20 @@ function App() {
         overflow: 'hidden'
     }}>
         <div style={{
-            
-            display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: (windowHeight / 2), transform: 'translateY(-102px)', paddingBottom: (windowHeight / 2)
+            display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: (windowHeight / 2), transform: 'translateY(-308.5px)', paddingBottom: (windowHeight / 2)
         }}>
             <div>
                 <div className = "ProfileArea" style={{
-                    color: '#66FCF1'}}>
+                    color: '#66FCF1', maxWidth: '50vw'}}>
                     <div className = "Introduction" >
-                <h1> Name</h1> <p> Text about me (height) {windowHeight} (width) {windowWidth} </p> <h2> Cool Stuff I did </h2> 
+                <h1 style={{
+                    fontFamily: 'Disket-Mono', fontSize: "60px"}}> 
+                    {WEBSITE_TEXT.NAME}</h1> 
+                <p style={{fontFamily: 'Kernel-Panic', fontSize: "30px"}}> {WEBSITE_TEXT.ABOUT_ME}</p> 
+                <button onClick={executeScroll} style={{
+                    backgroundColor: "transparent", color: "#66FCF1", border: "2px solid #66FCF1"
+                }}>
+                    {WEBSITE_TEXT.PROJECT_BUTTON}</button>{' '}
                     </div>
                     <div>
                     <img className = "picture" src={profilePicture}></img>
@@ -36,7 +54,7 @@ function App() {
                 </div>
             </div>
         </div>
-        <div style={{
+        <div ref={projectRef}  style={{
             color: '#C5C6C7', marginLeft: '10vh'
         }}>
             <h1>Projects</h1>
---
+++ src/strings/websiteText.js
@@ -0,0 +1,7 @@
+const WEBSITE_TEXT = {
+    NAME: "Hi, I'm Joel",
+    ABOUT_ME: "I'm a software engineering student at Rochester Institute of Technology who loves learning through projects. I have developed several apps for generating and accessing databases which prioritize usability. ",
+    PROJECT_BUTTON: "Check out my Projects"
+}
+
+export default WEBSITE_TEXT;

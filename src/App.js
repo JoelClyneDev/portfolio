44e376b2d8b9eb68752d9d1a12adc217937d9529
JoelClyneDev <joel.clyne2@gmail.com>
2020-07-23 22:45:31 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import profilePicture from './Red.svg.png';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    return ( 
+        <div>
+    <div className = "ProfileArea" style={{color: 'blue'}}>
+        <div className = "Introduction" >
+        <h1> Name</h1> <p> Text about me </p> <h2> Cool Stuff I did </h2> 
+        </div>
+        <div>
+        <img className = "picture" src={profilePicture}></img>
+        </div> 
     </div>
-  );
+
+    </div>
+    );
 }
 
 export default App;

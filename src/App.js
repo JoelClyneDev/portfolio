938817c8438b08d634b7d8a1b77cacc31d0d1932
JoelClyneDev <joel.clyne2@gmail.com>
2020-07-24 01:02:47 -0400

    fixed profile area alignment

--- src/App.js
+++ src/App.js
@@ -7,17 +7,48 @@ import profilePicture from './Red.svg.png';
 
 function App() {
 
-    return ( 
-        <div>
-    <div className = "ProfileArea" style={{color: 'blue'}}>
-        <div className = "Introduction" >
-        <h1> Name</h1> <p> Text about me </p> <h2> Cool Stuff I did </h2> 
-        </div>
-        <div>
-        <img className = "picture" src={profilePicture}></img>
-        </div> 
-    </div>
 
+    const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth
+    const profileAreaHeightForTranslation = 204 / 2;
+
+
+    return ( 
+    <div style={{
+        height: 'auto',
+        backgroundCOlor: '#1F2833',
+        margin: '0%',
+        overflow: 'hidden'
+    }}>
+        <div style={{
+            
+            display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: (windowHeight / 2), transform: 'translateY(-102px)', paddingBottom: (windowHeight / 2)
+        }}>
+            <div>
+                <div className = "ProfileArea" style={{
+                    color: '#66FCF1'}}>
+                    <div className = "Introduction" >
+                <h1> Name</h1> <p> Text about me (height) {windowHeight} (width) {windowWidth} </p> <h2> Cool Stuff I did </h2> 
+                    </div>
+                    <div>
+                    <img className = "picture" src={profilePicture}></img>
+                    </div> 
+                </div>
+            </div>
+        </div>
+        <div style={{
+            color: '#C5C6C7', marginLeft: '10vh'
+        }}>
+            <h1>Projects</h1>
+            <ul>
+                <li>
+                    Project 1
+                </li>
+                <li>
+                    Project 2
+                </li>
+            </ul>
+        </div>
     </div>
     );
 }
